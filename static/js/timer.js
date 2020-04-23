d6b3f97c7c532cf5bb4c37ebf80e19c7a97686b7
MegY <meghdadyazdi@yahoo.com>
2020-04-23 10:42:48 +0000

    fixing timer to count down and stop at 00 00 00 00

---
+++ static/js/timer.js
@@ -0,0 +1,53 @@
+var nowFix = new Date();
+nowFix = (Date.parse(nowFix) / 1000)+6;
+
+var days; 
+var hours;
+var minutes;
+var seconds;
+
+function makeTimer() {
+
+	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
+		
+            
+
+			var now = new Date();
+            now = (Date.parse(now) / 1000);
+            
+            //var endTime = new Date("29 April 2020 15:31:30 GMT+01:00");			
+            //endTime = (Date.parse(endTime) / 1000);
+            //endTime = 3600;
+
+            //var timeLeft = endTime - now;
+            var timeLeft = nowFix - now;
+           
+            console.log(timeLeft)
+            
+
+			days = Math.floor(timeLeft / 86400); 
+			hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+  
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+            if (seconds < "10") { seconds = "0" + seconds; }
+
+            if (hours >= 0 && minutes >= 0 && seconds >= 0 && days >= 0){
+                $("#days").html(days + "<span class='digits'>Days</span>");
+                $("#hours").html(hours + "<span class='digits'>Hours</span>");
+                $("#minutes").html(minutes + "<span class='digits'>Minutes</span>");
+                $("#seconds").html(seconds + "<span class='digits'>Seconds</span>");
+            }
+
+                
+
+    }
+    
+    console.log(seconds)
+
+    
+    setInterval(function() { makeTimer(); }, 1000);
+    
+    
