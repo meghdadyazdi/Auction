6deeb30ddc1ff11d7b793bd5da296b78aeb69fd0
MegY <meghdadyazdi@yahoo.com>
2020-05-08 17:43:58 +0000

    Fixing elif in start_Auction veiw.py

--- start_Auction/views.py
+++ start_Auction/views.py
@@ -9,14 +9,14 @@ def start_auction(request, pk):
     start the auction
     """
     item = get_object_or_404(Item, pk=pk)
-    item.auction_status = 1
     if (request.POST.get('end-time-fix-form') and (request.POST.get('auction-time-form'))):
         item.auction_end_time = request.POST.get('end-time-fix-form')
         item.auction_duration_time = request.POST.get('auction-time-form')
         item.highest_bid_offer = item.price
+        item.auction_status = 1
         print(item.auction_end_time)
         print(item.auction_duration_time)
-    if (request.POST.get('higher-bid-user') and (request.POST.get('higher-bid'))):
+    elif (request.POST.get('higher-bid-user') and (request.POST.get('higher-bid'))):
         bid = int(request.POST.get('higher-bid'))
         item.highest_bid_offer = item.highest_bid_offer + bid
         item.highest_bid_user = request.POST.get('higher-bid-user')
