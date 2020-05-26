7a3e96d1e39933104416e89701aed0e3d05090ff
MegY <meghdadyazdi@yahoo.com>
2020-05-26 09:29:06 +0000

    ADD ALLOWED HOST FOR HEROKU

--- Auction_website/settings.py
+++ Auction_website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['auction-meghdad.herokuapp.com']
 
 
 # Application definition
