5a5c4b269b1e9bd5dfdfae4968b0b2557533ee67
MegY <meghdadyazdi@yahoo.com>
2020-05-26 11:12:06 +0000

    add localhost

--- Auction_website/settings.py
+++ Auction_website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['auction-meghdad.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'auction-meghdad.herokuapp.com']
 
 
 # Application definition
