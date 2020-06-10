31a8aacadaeecdefbfb1ea4275f2eac5b4388055
MegY <meghdadyazdi@yahoo.com>
2020-06-10 10:49:29 +0000

    fixing migrate issue by setting default=1

--- payment/migrations/0002_order_winner_id.py
+++ payment/migrations/0002_order_winner_id.py
@@ -18,6 +18,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='order',
             name='winner_id',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
