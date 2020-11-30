e9d3223fc750b4b4d9cbfa168b24d2771a915edb
MegY <meghdadyazdi@yahoo.com>
2020-11-30 16:54:41 +0000

    remove git ignore

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_pUtm7CcRnJtpPGZU2P2LGA7J00xbLcmvKe")
+os.environ.setdefault("STRIPE_SECRET", "sk_test_euPPsiaeWoTX4WKdq8UEObSx00Hqi4ZSP6")
+os.environ.setdefault("DATABASE_URL", "postgres://qqruwdvjoguway:a9f905e820e7ce8d30141d2a35610fb5aa64f7096641be51ccf81443bb40641c@ec2-46-137-123-136.eu-west-1.compute.amazonaws.com:5432/de0gktc4118eep")
+os.environ.setdefault("SECRET_KEY", "^l74ns4-jvy))tdxz^^uqb6o^=5x1dv*io)ev6ufvr%uy!drh1")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIA3C6V5L5BKYZMJKFH")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
